f726f7892d07ff1e7fecfb10a16a59f5318ce988
Aravindvenkatesh <aravindkumarv841@gmail.com>
2018-05-30 15:12:49 +0530

    hi

---
+++ loop/src/loop/loop.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class loop {
+	public static void main(String args[]) {
+   int i = 0;
+	 do {
+		 System.out.println("Do while i = "+i);
+		 ++i;
+	 }while(i<10);
+	
+	 for(i = 0;i < 10; i++)
+	 System.out.println("for loop"+i);
+	}
+
+}
