000b0818cb014f005273f99cec8ff9ee8ed6998c
Aravindvenkatesh <aravindkumarv841@gmail.com>
2018-06-07 09:54:59 +0530

    java

---
+++ par/ParseNumber.java
@@ -0,0 +1,20 @@
+package com.par;
+
+public class ParseNumber {
+	private static String str;
+
+	public static void main(String args[]){
+			
+
+		    
+
+		    double number1=Double.parseDouble(args[0]);
+			double number2=Double.parseDouble(args[1]);
+			double sum=number1+number2;
+			System.out.println("Sum :"+sum);
+			
+			
+
+}
+
+}
