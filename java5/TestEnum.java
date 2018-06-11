9906345515a0ecb8e30940279dac5bcff482beb0
Aravindvenkatesh <aravindkumarv841@gmail.com>
2018-06-11 12:47:39 +0530

    enum

---
+++ java5/TestEnum.java
@@ -0,0 +1,17 @@
+package com.java.java5;
+
+public class TestEnum {
+	public static void main(String[] args) {
+		Level level = Level.HIGH;
+	    System.out.println(level.getClass().getName());
+	    System.out.println(level.name());
+	    System.out.println(level.ordinal());
+	    System.out.println(level.levelCode);
+	    System.out.println(level.getLevelCode());
+	    
+   Level levelOne[] = Level.values();
+   for(int i = 0; i < levelOne.length; i++) {
+	   System.out.println(levelOne);
+   }
+}
+}
